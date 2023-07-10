f4061316f139fd3ef4bc8f12e9e6a16054d20770
kouhei0128 <kouhei1412@outlook.jp>
2023-07-11 07:47:22 +0900

    add subcommand rs-client and some option

--- laughsmile.py
+++ laughsmile.py
@@ -16,6 +16,8 @@ def main():
     parser_synflood.add_argument('-p', '--port', help='target port')
     
     # reverceshell コマンドの parser を作成
+    parser_reverceshell_client = sub_psr.add_parser('rs-client', help='see `rs-client -h`')
+    parser_reverceshell_client.add_argument('target', help='target ip address')
     
     args = psr.parse_args()
     
@@ -26,7 +28,11 @@ def main():
             args.port = 80
         # SYN Flood攻撃
         syn_flood.syn_flood_attack(args.target, args.port)
-        
+    # サブコマンドが rs-client の場合(reverceshell)
+    elif args.subcommand == 'rs-client':
+        # client 要求
+        client.client(args.target)
+            
     # キーロガー
     #keylogger.keylogger()
     
